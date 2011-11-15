f41ac81fbfc98d66b094fe804842211eb6c11c00
Alfred Perlstein <alfred@freebsd.org>
2011-11-15 07:54:30 -0800

    Add functions to switch between kernel/user mode writevv

--- libwritevv/writevv.c
+++ libwritevv/writevv.c
@@ -1,14 +1,16 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-
 #include <sys/types.h>
 #include <sys/sysctl.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <strings.h>
+#include <errno.h>
 
 
 
@@ -63,8 +65,44 @@ getsyscallnum(SYSCALLVAR *syscallnum)
 
 static SYSCALLVAR writevv_syscall = SYSCALLVAR_INITIALIZER;
 
+static int writevv_mode = WRITEVV_MODE_KERNEL;
+
+int
+writevv_control(int op, int *val)
+{
+	int oldmode;
+
+	switch (op) {
+	case WRITEVV_GETMODE:
+		*val = writevv_mode;
+		return 0;
+	case WRITEVV_SETMODE:
+		oldmode = writevv_mode;
+		if (*val != WRITEVV_MODE_KERNEL && *val != WRITEVV_MODE_USER) {
+			errno = EINVAL;
+			return -1;
+		}
+		writevv_mode = *val;
+		return 0;
+	default:
+		errno = EINVAL;
+		return -1;
+	}
+}
+
 int
 writevv(const int *fds, int fdcnt, const struct iovec *iov, int iovcnt,
+    size_t *returns, int *errors)
+{
+	if (writevv_mode == WRITEVV_MODE_KERNEL)
+	    return writevv_kernel(fds, fdcnt, iov, iovcnt, returns, errors);
+	if (writevv_mode == WRITEVV_MODE_USER) 
+	    return writevv_userland(fds, fdcnt, iov, iovcnt, returns, errors);
+	abort();
+}
+
+int
+writevv_kernel(const int *fds, int fdcnt, const struct iovec *iov, int iovcnt,
     size_t *returns, int *errors)
 {
 	struct writevv_args args;
@@ -83,4 +121,18 @@ writevv(const int *fds, int fdcnt, const struct iovec *iov, int iovcnt,
 	args.returns = returns;
 	args.errors = errors;
 	return syscall(getsyscallnum(&writevv_syscall), args);
+}
+
+int
+writevv_userland(const int *fds, int fdcnt, const struct iovec *iov, int iovcnt,
+    size_t *returns, int *errors)
+{
+	int i, error;
+
+	for (i = 0; i < fdcnt; i++) {
+		error = writev(fds[i], iov, iovcnt);
+		returns[i] = error;
+		errors[i] = (error == -1) ? errno : 0;
+	}
+	return 0;
 }
--- libwritevv/writevv.h
+++ libwritevv/writevv.h
@@ -15,9 +15,23 @@ struct writevv_args {
 #endif
 
 #ifndef _KERNEL
-int
-writevv(const int *fds, int fdcnt, const struct iovec *iov, int iovcnt,
+int writevv(const int *fds, int fdcnt, const struct iovec *iov, int iovcnt,
     size_t *returns, int *errors);
+int writevv_kernel(const int *fds, int fdcnt,
+    const struct iovec *iov, int iovcnt,
+    size_t *returns, int *errors);
+int writevv_userland(const int *fds, int fdcnt,
+    const struct iovec *iov, int iovcnt,
+    size_t *returns, int *errors);
+
+#define WRITEVV_GETMODE	1
+#define WRITEVV_SETMODE	2
+
+#define WRITEVV_MODE_KERNEL	10
+#define WRITEVV_MODE_USER	11
+
+int writevv_control(int op, int *val);
+
 #endif
 
 #endif
